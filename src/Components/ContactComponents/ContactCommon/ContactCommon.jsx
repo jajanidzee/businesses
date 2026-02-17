95fcdc9e691c987cbfde6fdc4b53693cef5ea2e1
jajanidzee <jajanidzekote310@gmail.com>
2026-02-17 14:37:34 +0400

    k

--- src/Components/ContactComponents/ContactCommon/ContactCommon.jsx
+++ src/Components/ContactComponents/ContactCommon/ContactCommon.jsx
@@ -4,15 +4,15 @@ import { motion } from "motion/react";
 const ContactCommon = () => {
   return (
     <div className="ml-[35%] mt-[201.59px]">
-      <div className="flex items-end ">
-        <h1 className="text-[121.43px] text-[#141414] font-medium font-[Inter] ">
+      <div className="flex items-end gap-[30px] ">
+        <h1 className="text-[121.43px] text-[#141414] font-medium font-[Inter] leading-[80%] ">
           COMMON
         </h1>
-        <p className="text-[14.4px] text-[#141414] font-medium font-[Inter]  ">
+        <p className="text-[14.4px] text-[#141414] font-medium font-[Inter] leading-[100%]   ">
           SOME QUESTIONS <br /> PEOPLE USUALLY ASK
         </p>
       </div>
-      <h1 className="text-[121.43px] text-[#141414] font-medium font-[Inter] leading-[20%]">
+      <h1 className="text-[121.43px] text-[#141414] font-medium font-[Inter] leading-[100%]">
         QUESTIONS
       </h1>
       <div className="mt-[81.25px] max-w-[907.19px]">
